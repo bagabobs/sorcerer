84bf96e2a498a25fd6b3a893f6598ff9ba85326a
baga gusdiana <bagaman83@gmail.com>
2024-01-18 08:39:32 +0700

    initial commit

---
+++ src/test/java/com/bagabobs/sorcerer/SorcererApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bagabobs.sorcerer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SorcererApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
