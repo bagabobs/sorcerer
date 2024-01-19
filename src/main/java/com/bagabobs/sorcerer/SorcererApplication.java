d9e0f18158bd2924132024f34aba31371a32ff67
baga gusdiana <bagaman83@gmail.com>
2024-01-19 15:50:00 +0700

    Update config to allow CORS (#3)
    * test done
    
    * config for cors

--- src/main/java/com/bagabobs/sorcerer/SorcererApplication.java
+++ src/main/java/com/bagabobs/sorcerer/SorcererApplication.java
@@ -2,6 +2,9 @@ package com.bagabobs.sorcerer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class SorcererApplication {
@@ -9,5 +12,4 @@ public class SorcererApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SorcererApplication.class, args);
 	}
-
 }
